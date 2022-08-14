89def25c347ce00016d09e88bf95cdd52d0a4084
yvxrei <aylo7x@gmail.com>
2022-08-14 15:49:44 +0000

    1.0

--- generateImage.js
+++ generateImage.js
@@ -1,6 +1,6 @@
 const Canvas = require("canvas")
 const Discord = require("discord.js")
-const background = "https://i.imgur.com/zvWTUVu.jpg"
+const background = "attempt.jpg" 
 
 const dim = {
     height: 675,
@@ -17,7 +17,7 @@ const av = {
 const generateImage = async (member) => {
     let username = member.user.username
     let discrim = member.user.discriminator
-    let avatarURL = member.user.displayAvatarURL({format: "png", dynamic: false, size: av.size})
+    let avatarURL = member.user.displayAvatarURL({extension: "png", dynamic: false, size: av.size})
 
     const canvas = Canvas.createCanvas(dim.width, dim.height)
     const ctx = canvas.getContext("2d")
@@ -33,13 +33,39 @@ const generateImage = async (member) => {
     const avimg = await Canvas.loadImage(avatarURL)
     ctx.save()
     
+    //comment if you want square pfp
+
+ /*
+   ctx.beginPath()
+   ctx.arc(av.x + av.size * 2 , av.y + av.size * 2, av.size * 2, 0,2 * Math.PI , true)
+   ctx.closePath()
+   ctx.clip()
+
+    ctx.drawImage(avimg, av.x, av.y)
+    ctx.restore() 
+*/
+ctx.save()
+
+async function drawUserAvatar(ctx, avatar, avatar) {
+    const arcX = avatar.x + (avatar.w / 2)
+    const arcY = avatar.y + (avatar.h / 2)
+
     ctx.beginPath()
-    ctx.arc(av.x + av.size / 2, av.y + av.size / 2, av.size / 2, 0, Math.PI * 2, true)
+    ctx.arc(arcX, arcY, (avatar.w + 3) / 2, 0, Math.PI * 2, true)
+    ctx.closePath()
+    ctx.fillStyle = '#242424'
+    ctx.fill()
+    ctx.beginPath()
+    ctx.arc(arcX, arcY, avatar.w / 2, 0, Math.PI * 2, true)
     ctx.closePath()
     ctx.clip()
 
-    ctx.drawImage(avimg, av.x, av.y)
-    ctx.restore()
+    const avImg = await Canvas.loadImage(member.user.displayAvatarURL({ extension: "png" }))
+
+    ctx.drawImage(avImg, avatar.x, avatar.y, avatar.w, avatar.h)
+}      
+await drawUserAvatar(ctx, member, { w: av.size, h: av.size, x: av.x, y: av.y })
+ctx.restore()
 
     // write in text
     ctx.fillStyle = "white"
@@ -51,13 +77,13 @@ const generateImage = async (member) => {
 
     // draw in the username
     ctx.font = "60px Roboto"
-    ctx.fillText(username + discrim, dim.width/2, dim.height - dim.margin - 125)
+    ctx.fillText(username , dim.width/2, dim.height - dim.margin - 125)
 
     // draw in to the server
     ctx.font = "40px Roboto"
     ctx.fillText("to the server", dim.width / 2, dim.height - dim.margin - 50)
 
-    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcome.png")
+    const attachment = new Discord.AttachmentBuilder(canvas.toBuffer(), "welcome.jpg")
     return attachment
 }
 
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-//const generateImage = require ("./generateImage")
+const generateImage = require ("./generateImage")
 
     const client = new Discord.Client({
         intents: [
@@ -11,6 +11,10 @@ require("dotenv").config()
             "GuildMembers"
         ]
     })
+
+    let bot = {
+        client
+    }
 
     client.on("ready", () => {
         console.log(`Logged in as ${client.user.tag}`)
@@ -33,20 +37,32 @@ require("dotenv").config()
         }
     })
 
-    const welcomeChannelID = "976057748166950913"
+    client.slashcommands = new Discord.Collection() 
 
-    client.on("guildMemberAdd", async (member) => {
-        member.guild.channels.cache.get(welcomeChannelId).send(`<@${member.id}> Welcome to the server!`)
+    client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
+    client.loadSlashCommands(bot, false)
+
+    client.on("interactionCreate", (interaction) => {
+        if (!interaction.isCommand()) return 
+        if (!interaction.inGuild()) return interaction.reply("This command can only be used in a server")
+    
+        const slashcmd = client.slashcommands.get(interaction.commandName)
+    
+        if (!slashcmd) return interaction.reply("Invalid slash command")
+    
+        if (slashcmd.perm && !interaction.member.permissions.has(slashcmd.perm))
+            return interaction.reply("You do not have permission for this command")
+    
+        slashcmd.run(client, interaction)
     })
 
-   /* client.on("guildMemberAdd", async (member) => {
+    const welcomeChannelId = "460400962545254400"
+
+    client.on("guildMemberAdd", async (member) => {
         const img = await generateImage(member)
         member.guild.channels.cache.get(welcomeChannelId).send({
             content: `<@${member.id}> Welcome to the server!`,
             files: [img]
         })
-    }) */
-
-    TOKEN="MTAwNjk0MDc4ODk2NzYwNDMxNA.GRZ5qN.9y1JmitN4uBBkV6MKJh5cg1LO_0qw3S138vU8Q"
-    client.login(TOKEN)
-   // client.login(process.env.TOKEN)
+    }) 
+client.login(process.env.TOKEN)
